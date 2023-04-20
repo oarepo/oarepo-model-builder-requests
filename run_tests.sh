83799e1ab095b0e3575ab99eaf9a4e789ffef657
Ronald Krist <ronald.krist@cesnet.cz>
2023-04-20 15:53:44 +0200

    test fix

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,6 @@ set -e
 MODEL="example_document"
 MODEL_VENV=".model_venv"
 # export OPENSEARCH_PORT=9400
-#cd $(dirname $0)/..
 if test -d $MODEL; then
 	rm -rf $MODEL
 fi
@@ -12,6 +11,8 @@ oarepo-compile-model ./tests/$MODEL.yaml --output-directory ./tests/$MODEL -vvv
 python3 -m venv $MODEL_VENV
 . $MODEL_VENV/bin/activate
 pip install -U setuptools pip wheel
+pip install 'oarepo>=11.0.26,<12.0.0'
+pip uninstall -y invenio_oauth2server
 pip install "./tests/test_custom_classes"
 pip install "./tests/$MODEL[tests]"
 cp ./tests/test_custom_action.py ./tests/example_document/tests/requests/test_custom_action.py
