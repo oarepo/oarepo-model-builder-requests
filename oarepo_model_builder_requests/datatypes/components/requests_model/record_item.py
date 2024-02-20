c3ae5e2981400fc284e2fe34ac6679c2579640ea
Ronald Krist <ronald.krist@cesnet.cz>
2024-02-20 12:37:03 +0100

    reocrd item components not inherited

--- oarepo_model_builder_requests/datatypes/components/requests_model/record_item.py
+++ oarepo_model_builder_requests/datatypes/components/requests_model/record_item.py
@@ -1,19 +1,16 @@
-from oarepo_model_builder.datatypes import ModelDataType
+from oarepo_model_builder.datatypes import ModelDataType, DataTypeComponent
 from oarepo_model_builder.datatypes.components import RecordItemModelComponent
 from oarepo_model_builder.datatypes.components.model.utils import set_default
 
 
-class RequestsRecordItemModelComponent(RecordItemModelComponent):
+class RequestsRecordItemModelComponent(DataTypeComponent):
     eligible_datatypes = [ModelDataType]
-    dependancy_remap = RecordItemModelComponent
+    depends_on = [RecordItemModelComponent]
 
     def before_model_prepare(self, datatype, *, context, **kwargs):
         record_item_config = set_default(datatype, "record-item", {})
-        user_components = "components" in record_item_config
-        super().before_model_prepare(datatype, context=context, **kwargs)
-        if not user_components:
-            record_item_config.setdefault("components", [])
-            record_item_config["components"] += [
-                "{{oarepo_requests.services.results.RequestsComponent}}()",
-                "{{oarepo_requests.services.results.RequestTypesComponent}}()",
-            ]
+        record_item_config.setdefault("components", [])
+        record_item_config["components"] += [
+            "{{oarepo_requests.services.results.RequestsComponent}}()",
+            "{{oarepo_requests.services.results.RequestTypesComponent}}()",
+        ]
