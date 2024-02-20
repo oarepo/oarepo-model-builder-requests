0cebe2939fd352c4d232e1ec9288f9e0aeb331aa
Ronald Krist <ronald.krist@cesnet.cz>
2024-02-20 12:26:29 +0100

    ma components not inherited

--- oarepo_model_builder_requests/datatypes/components/requests_model/marshmallow.py
+++ oarepo_model_builder_requests/datatypes/components/requests_model/marshmallow.py
@@ -1,20 +1,16 @@
 from typing import Dict
 
-from oarepo_model_builder.datatypes import ModelDataType
+from oarepo_model_builder.datatypes import ModelDataType, DataTypeComponent
 from oarepo_model_builder.datatypes.components.model.marshmallow import (
     MarshmallowModelComponent,
 )
 from oarepo_model_builder.datatypes.components.model.utils import set_default
 
 
-class RequestsMarshmallowModelComponent(MarshmallowModelComponent):
+class RequestsMarshmallowModelComponent(DataTypeComponent):
     eligible_datatypes = [ModelDataType]
-    dependency_remap = MarshmallowModelComponent
+    depends_on = [MarshmallowModelComponent]
 
     def before_model_prepare(self, datatype, *, context, **kwargs):
-        super().before_model_prepare(datatype, context=context, **kwargs)
         marshmallow: Dict = set_default(datatype, "marshmallow", {})
-        marshmallow.get("base-classes", []).append(
-            "oarepo_requests.services.schema.RequestsSchemaMixin"
-        )
-        print()
+        marshmallow.get("base-classes", []).insert(0, "oarepo_requests.services.schema.RequestsSchemaMixin")
--- oarepo_model_builder_requests/datatypes/components/requests_model/ui_marshmallow.py
+++ oarepo_model_builder_requests/datatypes/components/requests_model/ui_marshmallow.py
@@ -1,21 +1,18 @@
 from typing import Dict
 
-from oarepo_model_builder.datatypes import ModelDataType
+from oarepo_model_builder.datatypes import ModelDataType, DataTypeComponent
 from oarepo_model_builder.datatypes.components import UIMarshmallowModelComponent
 from oarepo_model_builder.datatypes.components.model.utils import set_default
 
 
-class RequestsUIMarshmallowModelComponent(UIMarshmallowModelComponent):
+class RequestsUIMarshmallowModelComponent(DataTypeComponent):
     eligible_datatypes = [ModelDataType]
-    dependency_remap = UIMarshmallowModelComponent
+    depends_on = [UIMarshmallowModelComponent]
 
     def before_model_prepare(self, datatype, *, context, **kwargs):
-        super().before_model_prepare(datatype, **kwargs)
-        marshmallow: Dict = set_default(datatype, "ui", "marshmallow", {})
-        # TODO this is in builtin models; what is the correct approach to override this?
-        append_ui_record_requests = True
-        if append_ui_record_requests:
-            marshmallow["base-classes"].append("oarepo_requests.services.ui_schema.UIRequestsSerializationMixin")
+        if datatype.root.profile in {"record", "draft"}:
+            marshmallow: Dict = set_default(datatype, "ui", "marshmallow", {})
+            marshmallow["base-classes"].insert(0, "oarepo_requests.services.ui_schema.UIRequestsSerializationMixin")
 
 
 
