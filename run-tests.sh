9f6845dae1fb0bfb01233f2d8f8ec3674bc6776e
Mirek Simek <miroslav.simek@cesnet.cz>
2024-06-11 15:29:46 +0200

    Always using record proxy from oarepo to pick more fields (#24)
    * Always using record proxy from oarepo
    
    * Building for v12 only
    
    * Removed local override

--- run-tests.sh
+++ run-tests.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -e
+set -x
 
 MODEL="thesis"
 TESTS_VENV=".venv-tests"
 CODE_TEST_DIR="tests"
 BUILD_TEST_DIR="build-tests"
 
-OAREPO_VERSION=${OAREPO_VERSION:-11}
+OAREPO_VERSION=${OAREPO_VERSION:-12}
 OAREPO_VERSION_MAX=$((OAREPO_VERSION+1))
+PYTHON="${PYTHON:-python3.10}"
 
 BUILDER_VENV=".venv-builder"
 if test -d $BUILDER_VENV ; then
 	rm -rf $BUILDER_VENV
 fi
 
-python3 -m venv $BUILDER_VENV
+${PYTHON} -m venv $BUILDER_VENV
 . $BUILDER_VENV/bin/activate
 pip install -U setuptools pip wheel
 pip install -e .
@@ -30,11 +32,15 @@ if test -d $TESTS_VENV; then
 	rm -rf $TESTS_VENV
 fi
 
-python3 -m venv $TESTS_VENV
+${PYTHON} -m venv $TESTS_VENV
 . $TESTS_VENV/bin/activate
 pip install -U setuptools pip wheel
 pip install "oarepo>=$OAREPO_VERSION,<$OAREPO_VERSION_MAX"
 pip install "./$BUILD_TEST_DIR/${MODEL}[tests]"
 pip install "./$CODE_TEST_DIR/test_custom_classes"
 cp -r ./$CODE_TEST_DIR/requests_tests ./$BUILD_TEST_DIR/$MODEL/tests/requests
-# pytest $BUILD_TEST_DIR/$MODEL/tests
+
+## local override
+#pip install -e ../oarepo-runtime
+
+pytest $BUILD_TEST_DIR/$MODEL/tests
