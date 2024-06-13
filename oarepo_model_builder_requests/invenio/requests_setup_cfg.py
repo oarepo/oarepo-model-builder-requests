9a8e565d9ffccce93950aba77a0376339f809a60
Mirek Simek <miroslav.simek@cesnet.cz>
2024-06-13 21:08:55 +0200

    Removed obsolete model elements - types and actions (#27)

--- oarepo_model_builder_requests/invenio/requests_setup_cfg.py
+++ oarepo_model_builder_requests/invenio/requests_setup_cfg.py
@@ -12,19 +12,3 @@ class RequestsSetupCfgBuilder(OutputBuilder):
         output: CFGOutput = self.builder.get_output("cfg", "setup.cfg")
 
         output.add_dependency("oarepo-requests", ">=1.0.2")
-        if (
-            "requests" not in self.current_model.definition
-            or not self.current_model.definition["requests"]
-            or "types" not in self.current_model.definition["requests"]
-        ):
-            return
-        requests = self.current_model.definition["requests"]["types"]
-        if requests:
-            for request_data in requests.values():
-                class_ = request_data["class"]
-                module, base = split_package_base_name(class_)
-                output.add_entry_point(
-                    "invenio_requests.types",
-                    request_data["id"],
-                    f"{module}:{base}",
-                )
