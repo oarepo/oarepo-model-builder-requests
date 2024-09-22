bbd07694545c47ec4c6febb07b98a1a1d6abbc79
Mirek Simek <miroslav.simek@gmail.com>
2024-09-22 16:56:28 +0200

    Enabling back tests

--- run-tests.sh
+++ run-tests.sh
@@ -42,4 +42,5 @@ cp -r ./$CODE_TEST_DIR/requests_tests ./$BUILD_TEST_DIR/$MODEL/tests/requests
 
 ## local override
 #pip install -e ../oarepo-runtime
-# pytest tests/requests_tests
+
+pytest tests/requests_tests
